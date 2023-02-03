1a9c3bfc3094ed8f850746f306a29b7b02d99045
Yari Herrera <yari.andres@gmail.com>
2023-02-03 15:26:21 +0100

    adds blog and home page style changes and creates custome component

---
+++ components/BlogCardItem.vue
@@ -0,0 +1,74 @@
+<template>
+  <div :class="$style['card']">
+    <figure :class="$style['card__figure']">
+      <img :class="['card__image']" :src="image ? image : 'https://placeholder.pics/svg/248x298'" alt="ramdom image">
+    </figure>
+
+    <div :class="$style['card__body']">
+      <p :class="$style['card__body--date']">{{ date }}</p>
+      <h2 :class="$style['card__body--title']">{{ title }}</h2>
+      <NuxtLink :to="to">
+        <span :class="$style['card__body--button']">Read</span>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  defineProps<
+    {
+      to: string;
+      title: string;
+      date: string;
+      image?: string;
+      description: string;
+    }
+  >();
+</script>
+
+<style lang="scss" module>
+ .card {
+    display: flex;
+    gap: 2.9rem;
+
+    &__image {
+      border-radius: 5px 5px 0 0;
+    }
+
+    &__body {
+      padding: 1.6rem 0;
+      width: 15.9rem;
+      display: flex;
+      flex-direction: column;
+      gap: 1.6rem;
+
+      &--date {
+        font-size: 1.2rem;
+        font-weight: 100;
+      }
+
+      &--title {
+        font-size: 2.6rem;
+        font-weight: 100;
+      }
+
+      &--button {
+        background-color: #000;
+        border: none;
+        border-radius: 2rem;
+        color: #ffffff;
+        font-size: 1.2rem;
+        font-weight: 800;
+        cursor: pointer;
+        padding: 0.9rem 2rem;
+        transition: box-shadow 0.3s, background-color 0.3s ease-in-out, color 0.2s ease-in-out;
+        
+        &:hover {
+          background-color: #ffffff;
+          color: #000;
+          box-shadow: 0px 2px 25px rgba(0, 0, 0, 0.2);
+        }
+      }
+    }
+  }
+</style>
---
+++ components/MainWrapper.vue
@@ -0,0 +1,15 @@
+<template>
+  <div :class="$style['main']">
+    <slot/>
+  </div>
+</template>
+
+<style lang="scss" module>
+.main {
+    display: grid;
+    grid-template-columns: 3fr 3fr;
+    background-color: #D0CBC5;
+    border-radius: 1rem;
+    padding: 5.6rem 0;
+}
+</style>
