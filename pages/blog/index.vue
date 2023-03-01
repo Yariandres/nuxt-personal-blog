1b0d290e530d11a1be99c243ba3174aef58af685
Yari Herrera <yari.andres@gmail.com>
2023-03-01 22:22:48 +0100

    adds site nav

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="text-white">
     Blog page component
   </div>
 </template>
--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="text-white">
     conatct page component
   </div>
 </template>
