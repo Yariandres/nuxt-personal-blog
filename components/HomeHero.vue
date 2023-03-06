5ce1f7fbbadec868e817677a915cd7248976088d
Yari Herrera <yari.andres@gmail.com>
2023-03-06 23:12:49 +0100

    adds blog article

--- components/HomeHero.vue
+++ components/HomeHero.vue
@@ -1,11 +1,13 @@
 <template>
-    <div :class="$style['hero']">
-    <div :class="$style['holder']">
-      <img src="~/assets/img/yari.jpeg" alt="thum nail" />
+  <div :class="$style['hero']">
+
+    <div :class="$style['row']">
+      <div :class="$style['holder']">
+        <img src="~/assets/img/yari.jpeg" alt="thum nail" />
+      </div>
+      <p :class="$style['header']">Hello! I am Yari and since 7 years I <span> have </span> been creating robust scalable <span>commercial</span> web software that achieve specific goals
+      </p>
     </div>
-    <p :class="$style['header']">Hello! I am Yari and since 7 years I have been <span>creating robust scalable commercial
-        web software that achieve specific goals</span>
-    </p>
 
     <div :class="$style['copy']">
       <p :class="$style['text']">Ready to launch you next idea? Working with me is like having a personal coding ninja at
@@ -28,33 +30,39 @@
 .hero {
   padding-block: 7rem;
 
-  .holder {
-    height: 163px;
-    width: 163px;
-    border-radius: 50%;
-    overflow: hidden;
-    float: left;
-    margin-right: 1rem;
 
-    img {
-      height: 100%;
-      width: 100%;
-      object-fit: cover;
+  .row {
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+    .holder {
+      height: 80%;
+      width: 80%;
+      border-radius: 50%;
+      overflow: hidden;
+
+      img {
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
+      }
     }
-  }
 
-  .header {
-    font-size: 4.2rem;
-    display: inline;
-    line-height: 1.4;
+    .header {
+      font-size: 4.6rem;
+      display: inline;
+      line-height: 1.4;
 
-    span {
-      margin-left: 1.9rem;
+      span {
+        color: var(--text-color-active);
+        margin-left: 2rem;
+      }
+
     }
   }
 
   .text {
-    font-size: 1.9rem;
+    font-size: 2.3rem;
     margin-top: 5rem;
     max-width: 70.5rem;
     letter-spacing: 1px;
@@ -63,7 +71,7 @@
   .copy {
     display: flex;
     flex-direction: column;
-    gap: 5rem;
+    gap: 2rem;
 
     .icons {
       display: flex;
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,46 @@
 <template>
-  <HomeHero />
+  <main>
+    <HomeHero />
+    <div :class="$style['row']">
+      <article :class="$style['article']">
+        <p>September 5, 2023</p>
+        <h2>Crafting a design system for the multidisciplinary future</h2>
+        <p>Most companies try to stay ahead of the curve when it comes to visual design, but for Planetaria we needed to create a brand that would still inspire us 100 years from now when humanity has spread across our entire solar system.</p>
+        <NuxtLink to="#">Read article</NuxtLink>
+      </article>
+
+      <aside :class="$style['aside']">
+        aside section
+      </aside>
+
+    </div>
+  </main>
 </template>
 
 <script setup lang="ts">
 
 </script>
+
+<style lang="scss" module>
+.row {
+  display: flex;
+  gap: 2rem;
+
+  .article {
+    flex: 3;
+    padding: 2rem;
+    border: 1px solid transparent;
+    border-radius: 2rem;
+
+    &:hover {
+      background-color: #474545;
+      transition: background-color 0.5s ease;
+    }
+  }
+
+  .aside {
+    flex: 2;
+  }
+}
+</style>
 
