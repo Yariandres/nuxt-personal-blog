9626e4afb0170b8dacf836a000de9ae6ef9cacb5
Yari Herrera <yari.andres@gmail.com>
2023-03-13 22:13:51 +0100

    adds base layout fot the entire app

---
+++ components/BlogFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="$style['footer']">
+    <h2>Yari's blog &copy; ALl Rights Reserved {{ year }}</h2> 
+  </div>
+</template>
+
+<script setup lang="ts">
+// get today's date
+const today = new Date();
+const year = today.getFullYear();
+</script>
+
+<style lang="scss" module>
+.footer {
+  color: #fff;
+  padding: 1rem;
+  text-align: center;
+}
+</style>
---
+++ components/Layout/Main.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :class="$style['main-layout']">
+    <slot name="navigation"/>
+
+    <div :class="$style['main-layout__main']">
+      <slot name="main"/>
+    </div>
+    <slot name="footer"/>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" module>
+.main-layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100dvh;
+
+  &__main {
+    flex-grow: 1;
+  }
+}
+</style>
