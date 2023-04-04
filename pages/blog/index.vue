4db7daf6ee6396e157f99b7f2a5ec9a1bbc50684
Yari Herrera <yari.andres@gmail.com>
2023-04-04 23:12:02 +0200

    adds search and select sort fields to blog page

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,37 +1,50 @@
 <template>
   <div :class="$style['base-container']">
-    <h1>All posts</h1>
-    <div>
-      sort by:
-      <!-- <select v-model="sortBy"> -->
-      <select>
-        <option value="date">Date</option>
-        <option value="title">Title</option>
-      </select>
-    </div>
+    <LayoutFlexColumn :gap="2.8">
+      <h1 :class="$style['title']">All posts</h1>
+      <LayoutFlexRow>
+        <LayoutFlexColumn :gap="1">
+          <label for="search">Search post</label>
+          <input type="text" v-model="search" placeholder="Search posts" id="search"/>
+        </LayoutFlexColumn>
+
+        <LayoutFlexColumn :gap="1">
+          <label for="select">sort by:</label> 
+          <select v-model="sortBy" id="select">
+            <option value="latest">newest</option>
+            <option value="Oldest">Oldest</option>
+          </select>
+        </LayoutFlexColumn>
+      </LayoutFlexRow>
+    </LayoutFlexColumn>
   </div>
 </template>
 
 <script setup lang="ts">
 
-// TODO - sort by date and title
+const search = ref('');
+const sortBy = ref('latest');
 
-// const sortby = () => {
-//   if (sortBy === 'date') {
-//     return posts.value.sort((a, b) => {
-//       return new Date(b.date).getTime() - new Date(a.date).getTime();
-//     });
-//   } else if (sortBy === 'title') {
-//     return posts.value.sort((a, b) => {
-//       return a.title.rendered.localeCompare(b.title.rendered);
-//     });
-//   }
-// }
- </script>
+// get all posts
+const { data: posts } = await useWordpressApi().getPosts<any>();
+
+</script>
 
 <style lang="scss" module>
 .base-container {
-  padding-inline: var(--space-400);
+  padding-inline: 4rem;
 }
+
+.flex-column {
+  display: flex;
+  flex-direction: column;
+  gap: 2.8rem;
+}
+
+.title {
+    font-size: 2.4rem;
+    font-weight: 800;
+    text-align: center;
+  }
 
 </style>
