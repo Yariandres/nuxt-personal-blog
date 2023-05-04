10aea2956ca5cb3151a5c0dc9e6df724f605f1db
Yari Herrera <yari.andres@gmail.com>
2023-05-05 00:20:16 +0200

    adding modal card and dialog component

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -21,21 +21,25 @@
 
         <div :class="$style['card-row']">
           <div :class="$style['card']">
-            <div :class="$style['card__header']">
-              <p :class="$style['text-orange']">-2018 - Present</p>
-              <p>-Envato Market</p>
-            </div>
-            
-            <div :class="$style['flex-gap-16']">
-              <h3>Web Developer</h3>
-              <div :class="$style['card__text']">
-                <p>Website development is the process of building, programming, coding and maintaining websites and web applications.
-                </p>
+            <button :class="$style['card__btn']" @click="openModal">
+              <div :class="$style['card__header']">
+                <p :class="$style['text-orange']">-2018 - Present</p>
+                <p>-Envato Market</p>
               </div>
-            </div>
-            
-            <!-- Modalbox Info Start -->
-            <dialog>
+              
+              <div :class="$style['flex-gap-16']">
+                <h3 :class="$style['card__title']">Web Developer</h3>
+                <div :class="$style['card__text']">
+                  <p>Website development is the process of building, programming, coding and maintaining websites and web applications.
+                  </p>
+                </div>
+              </div>
+            </button>
+              
+              <!-- Modalbox Info Start -->
+            <dialog ref="modal" :class="$style['modal']">
+              <p @click="closeModal">Close Modal</p>
+
               <img :class="$style['popup_image']" src="~/assets/img/experience/1.jpg" alt="" />
               <div :class="$style['descriptions']">
                 <p>Elisc is a leading web design agency with an award-winning design team that creates innovative, effective websites that capture your brand, improve your conversion rates, and maximize your revenue to help grow your business and achieve your goals.</p>
@@ -51,6 +55,18 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const modal = ref<HTMLDialogElement | null>(null);
+
+const openModal = () => {
+  modal.value?.showModal();
+}
+
+const closeModal = () =>{
+  modal.value?.close();
+  console.log('close modal function')
+}
 
 </script>
 
@@ -96,6 +112,7 @@
   .experience {
     padding-inline: 90px;
     padding-block: 90px;
+
     .greetings {
       text-transform: uppercase;
       font-weight: 500;
@@ -123,16 +140,35 @@
       box-shadow: -1px 9px 25px -5px rgba(0,0,0,0.1);
       -webkit-box-shadow: -1px 9px 25px -5px rgba(0,0,0,0.1);
       -moz-box-shadow: -1px 9px 25px -5px rgba(0,0,0,0.1);
-      cursor: pointer;
+      
 
+      &__btn {
+        background: none;
+        border: none;
+        margin: 0;
+        padding: 0;
+        font: inherit;
+        color: inherit;
+        cursor: pointer;
+        outline: none;
+        text-align: left;
+      }
+      
       &__header {
         display: flex;
         justify-content: space-between;
+
         p {
           font-size: 14px;
           font-weight: 500;
         }
       }
+
+      &__title {
+        font-weight: 700;
+        font-size: 19px;
+      } 
+
       .font-style {
         font-size: 14px;
         font-weight: 400;
@@ -147,6 +183,16 @@
 
       .text-orange {
         color: orange;
+      }
+
+      .modal {
+        max-width: 80ch;
+        border-radius: 5px;
+        outline: none;
+
+        &::backdrop {
+          background: rgba(51, 51, 51, 0.928);
+        }
       }
     };
 
