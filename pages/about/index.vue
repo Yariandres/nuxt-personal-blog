9314fe84383b5152e81fc11c99ff7fe7fedd9a38
Yari Herrera <yari.andres@gmail.com>
2023-05-05 13:43:49 +0200

    adding modal

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -35,19 +35,23 @@
                 </div>
               </div>
             </button>
-              
-              <!-- Modalbox Info Start -->
-            <dialog ref="modal" :class="$style['modal']">
-              <p @click="closeModal">Close Modal</p>
-
-              <img :class="$style['popup_image']" src="~/assets/img/experience/1.jpg" alt="" />
-              <div :class="$style['descriptions']">
-                <p>Elisc is a leading web design agency with an award-winning design team that creates innovative, effective websites that capture your brand, improve your conversion rates, and maximize your revenue to help grow your business and achieve your goals.</p>
-                <p>In today’s digital world, your website is the first interaction consumers have with your business. That's why almost 95 percent of a user’s first impression relates to web design. It’s also why web design services can have an immense impact on your company’s bottom line.</p>
-                <p>That’s why more companies are not only reevaluating their website’s design but also partnering with Elisc, the web design agency that’s driven more than $2.4 billion in revenue for its clients. With over 50 web design awards under our belt, we're confident we can design a custom website that drives sales for your unique business.</p>
-              </div>
-            </dialog>
           </div>
+          <dialog ref="modal" :class="{[$style['modal']]: true, [$style['modal__isOpen']]: notOpen}">
+            <button :class="$style['modal__close-icon']" @click="closeModal">
+              <i class="icon-cancel"></i>
+            </button>
+
+            <img :class="$style['modal__image']"  src="~/assets/img/experience/1.jpg" alt="" />
+
+            <p :class="$style['modal__time']">-2018 - Present</p>
+            <p :class="$style['modal__company']">-Envato Market</p>
+
+            <div :class="$style['modal__description']">
+              <p>Elisc is a leading web design agency with an award-winning design team that creates innovative, effective websites that capture your brand, improve your conversion rates, and maximize your revenue to help grow your business and achieve your goals.</p>
+              <p>In today’s digital world, your website is the first interaction consumers have with your business. That's why almost 95 percent of a user’s first impression relates to web design. It’s also why web design services can have an immense impact on your company’s bottom line.</p>
+              <p>That’s why more companies are not only reevaluating their website’s design but also partnering with Elisc, the web design agency that’s driven more than $2.4 billion in revenue for its clients. With over 50 web design awards under our belt, we're confident we can design a custom website that drives sales for your unique business.</p>
+            </div>
+          </dialog>
         </div>
       </div>
     </div>
@@ -58,14 +62,16 @@
 import { ref } from 'vue';
 
 const modal = ref<HTMLDialogElement | null>(null);
+const notOpen = ref<boolean>(true);
 
 const openModal = () => {
   modal.value?.showModal();
+  notOpen.value = false;
 }
 
 const closeModal = () =>{
   modal.value?.close();
-  console.log('close modal function')
+  notOpen.value = true;
 }
 
 </script>
@@ -184,17 +190,93 @@ const closeModal = () =>{
       .text-orange {
         color: orange;
       }
+    };
 
-      .modal {
-        max-width: 80ch;
+    .modal {
+      max-width: 80ch;
+      border-radius: 5px;
+      padding-inline: 40px;
+      padding-block-start: 60px;
+      display: flex;
+      flex-direction: column;
+      gap: 22px;
+      position: relative;
+      isolation: isolate;
+      z-index: 1;
+
+      &__isOpen {
+        display: none;
+      }
+
+      &::backdrop {
+        background: rgba(51, 51, 51, 0.928);
+      }
+
+      &__image {
         border-radius: 5px;
-        outline: none;
+        height: 375px; // to be removed  
+      }
 
-        &::backdrop {
-          background: rgba(51, 51, 51, 0.928);
+      &__time {
+        background-color: #000000;
+        color: orange;
+        padding-block: 10px;
+        text-align: center;
+        border-radius: 5px;
+        display: inline;
+        max-width: 160px;
+        font-size: 16px;
+        font-weight: 400;
+      }
+
+      &__company {
+        font-size: 14px;
+        font-weight: 400;
+        line-height: 30px;
+        color: rgba(85, 82, 124, 0.501);
+      }
+
+      &__description {
+        font-size: 16px;
+        font-weight: 400;
+        line-height: 30px;
+        color: rgba(85, 82, 124, 0.632);
+      }
+
+      &__close-icon {
+        position: absolute;
+        right: 0;
+        top: 12px;
+        background: none;
+        border: none;
+        margin: 0;
+        padding: 0;
+        font: inherit;
+        color: inherit;
+        cursor: pointer;
+        outline: none;
+        
+
+        i {
+          padding: 10px;
+          color: rgba(85, 82, 124, 0.632);;
+          border: 2px solid rgba(85, 82, 124, 0.632);;
+          border-radius: 10px;
+          z-index: 10;
+
+          -webkit-transition: all 0.3s ease;
+          -moz-transition: all 0.3s ease;
+          -ms-transition: all 0.3s ease;
+          -o-transition: all 0.3s ease;
+          transition: all 0.3s ease;
+
+          &:hover {
+            border-radius: 100%;
+          }
         }
       }
-    };
+      
+    }
 
     .flex-gap-16 {
       display: flex;
