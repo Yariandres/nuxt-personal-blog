47b86b08b8ec80fa0fc61aba6a3550958f0a8070
Yari Herrera <yari.andres@gmail.com>
2023-08-07 23:35:10 +0200

    adds theme switcher icon

--- components/navigation/Bar.vue
+++ components/navigation/Bar.vue
@@ -9,7 +9,29 @@ const toggleLightTheme = () => {
 </script>
 
 <template>
-  <button @click="toggleLightTheme">Mode {{ mode }}</button>
+  <div :class="$style['switcher']">
+    <navigation-moon-icon v-if="mode === 'light'" @click="toggleLightTheme" />
+    <navigation-sun-icon
+      v-if="mode === 'dark'"
+      @click="toggleLightTheme"
+      :mode="mode"
+    />
+  </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss" module>
+.switcher {
+  display: flex;
+  justify-content: flex-end;
+  padding-inline: 40px;
+
+  svg {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    width: 50px;
+    z-index: 1;
+    cursor: pointer;
+  }
+}
+</style>
