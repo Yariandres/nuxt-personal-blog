c5ac8bf538f01085f6298d56025885708353880d
Yari Herrera <yari.andres@gmail.com>
2023-08-14 23:27:53 +0200

    adds form inputs with animations

--- components/pages/home/Form.vue
+++ components/pages/home/Form.vue
@@ -6,7 +6,7 @@
       <div class="form-items">
         <form class="form">
           <h2 class="heading">Say hello!</h2>
-          <div class="form-group">
+          <div class="form__group">
             <input
               type="text"
               class="form__input"
@@ -52,7 +52,7 @@
         rgba(#fff, 0.9) 50%,
         transparent 50%
       ),
-      url('~/assets/images/hero-light-mode.png');
+      url('~/assets/images/hero-dark-mode.png');
   }
 }
 
@@ -64,7 +64,7 @@
         rgba(#fff, 0.9) 50%,
         transparent 50%
       ),
-      url('~/assets/images/hero-dark-mode.png');
+      url('~/assets/images/hero-ligth-mode.png');
   }
 }
 
@@ -73,6 +73,7 @@
   font-weight: 100;
   text-transform: uppercase;
   color: #221520;
+  margin-bottom: 50px;
 }
 
 .form-container {
@@ -86,5 +87,54 @@
 .form-items {
   width: 50%;
   padding: 60px;
+}
+
+.form {
+  &__group:not(:last-child) {
+    margin-bottom: 20px;
+  }
+
+  &__input {
+    font-size: 15px;
+    padding: 15px 20px;
+    border-radius: 4px;
+    background-color: rgba(#fff, 0.5);
+    border: none;
+    font-family: inherit;
+    border-bottom: 3px solid transparent;
+    width: 70%;
+    display: block;
+    transition: all 0.3s;
+
+    &::-webkit-input-placeholder {
+      color: rgb(103, 103, 103);
+    }
+
+    &:focus {
+      outline: none;
+      box-shadow: 0 16px 20px rgba(black, 0.1);
+      border-bottom: 3px solid #01ff23;
+    }
+
+    &:focus:invalid {
+      border-bottom: 3px solid orangered;
+    }
+  }
+
+  &__label {
+    color: #221520;
+    font-size: 12px;
+    font-weight: 700;
+    margin-left: 20px;
+    margin-top: 0.7rem;
+    display: block;
+    transition: all 0.3s;
+  }
+
+  &__input:placeholder-shown + &__label {
+    opacity: 0;
+    visibility: hidden;
+    transform: translateY(-40px);
+  }
 }
 </style>
